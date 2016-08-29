0f8aef0dc99fa66eff018c00a37e71a4306360d2
Vasiliy Lector <vasiliy.lector@gmail.com>
2016-08-29 12:08:39 +0300

    coverage createReducer

--- src/createReducer.js
+++ src/createReducer.js
@@ -1,6 +1,9 @@
 import Store from './Store';
 
 export default function createReducer(action) {
+    if (typeof action !== 'function') {
+        throw new Error('Expect required parameter action to be typeof function!');
+    }
     return ({ store, payload }) => {
         if (!(store instanceof Store)) {
             throw new Error('Expect required parameter store to be instance of Store!');
