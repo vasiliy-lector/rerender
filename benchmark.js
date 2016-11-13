30d93d5f40ec948156b497b0b9f3fcb4ff270a3b
Vasiliy Lector <vasiliy.lector@gmail.com>
2016-11-13 14:09:55 +0300

    t7 benchmark added

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,7 @@
 var Benchmark = require('benchmark'),
+    t7 = require('t7'),
     html = require('./lib/html.js').default,
-    htmlNoCache = require('./lib/htmlNoCache.js').default;
+    htmlNoCache = require('./lib/html.js').htmlNoCache;
 
 var suite = new Benchmark.Suite,
     templates = ['<div class="block" style=',' id="id1" title=','><p id="id2" class=','>text of p</p></div>'],
@@ -14,12 +15,18 @@ function noCache() {
     return htmlNoCache(templates, values[0], values[1], values[2]);
 }
 
+function t7Fn() {
+    return t7(templates, values[0], values[1], values[2]);
+}
+
 console.log('noCache', noCache()); // eslint-disable-line no-console
 console.log('cache', cache()); // eslint-disable-line no-console
+console.log('t7Fn', t7Fn()); // eslint-disable-line no-console
 
 suite
 .add('noCache', noCache)
 .add('cache', cache)
+.add('t7Fn', t7Fn)
 .on('cycle', function(event) {
     console.log(String(event.target)); // eslint-disable-line no-console
 })
--- src/html.js
+++ src/html.js
@@ -11,112 +11,122 @@ import {
     deffered
 } from './parser';
 
-configure('cacheEnabled', true);
-configure('autoCacheEnabled', true);
-configure('autoCacheIndexEnabled', true);
-configure('autoCacheOptionalEnabled', true);
+function getRoot(useCache) {
+    configure('cacheEnabled', useCache);
+    configure('autoCacheEnabled', useCache);
+    configure('autoCacheIndexEnabled', useCache);
+    configure('autoCacheOptionalEnabled', useCache);
 
-const
-    whiteSpace = find(/^\s+/),
-    optionalWhiteSpace = optional(whiteSpace).useCache(),
-    textNode = find(/^[^<]+/),
-    tagName = find(/^[a-zA-Z][a-zA-Z0-9]*/),
-    placeholder = next().then((result, values) => values[result]),
-    attrName = find(/^[a-zA-Z_][a-zA-Z0-9]*/),
-    booleanAttr = attrName.then(result => [result, true]).useCache(),
-    quotedAttr = sequence(
-        attrName,
-        find('='),
-        required(find('"')),
-        find(/[^"]*/),
-        required(find('"'))
-    ).then(result => [result[0], result[3]]).useCache(),
-    attrWithPlaceholder = sequence(
-        attrName,
-        find('='),
-        any(
-            placeholder,
-            sequence(
-                required(find('"')),
+    const
+        whiteSpace = find(/^\s+/),
+        optionalWhiteSpace = optional(whiteSpace),
+        textNode = find(/^[^<]+/),
+        tagName = find(/^[a-zA-Z][a-zA-Z0-9]*/),
+        placeholder = next().then((result, values) => values[result]),
+        attrName = find(/^[a-zA-Z_][a-zA-Z0-9]*/),
+        booleanAttr = attrName.then(result => [result, true]).useCache(),
+        quotedAttr = sequence(
+            attrName,
+            find('='),
+            required(find('"')),
+            find(/[^"]*/),
+            required(find('"'))
+        ).then(result => [result[0], result[3]]).useCache(),
+        attrWithPlaceholder = sequence(
+            attrName,
+            find('='),
+            any(
                 placeholder,
-                required(find('"'))
-            ).then(result => result[1])
-        )
-    ).then(result => [result[0], result[2]]),
-    attrs = repeat(
-        any(
-            placeholder,
-            attrWithPlaceholder,
-            quotedAttr,
-            booleanAttr
-        ),
-        whiteSpace
-    ).then(results => {
-        const memo = {};
+                sequence(
+                    required(find('"')),
+                    placeholder,
+                    required(find('"'))
+                ).then(result => result[1])
+            )
+        ).then(result => [result[0], result[2]]),
+        attrs = repeat(
+            any(
+                placeholder,
+                attrWithPlaceholder,
+                quotedAttr,
+                booleanAttr
+            ),
+            whiteSpace
+        ).then(results => {
+            const memo = {};
 
-        for (let i = 0, l = results.length; i < l; i++) {
-            const result = results[i];
-            if (result[0]) {
-                memo[result[0]] = result[1];
-            } else {
-                const keys = Object.keys(result);
-                let j = keys.length;
+            for (let i = 0, l = results.length; i < l; i++) {
+                const result = results[i];
+                if (result[0]) {
+                    memo[result[0]] = result[1];
+                } else {
+                    const keys = Object.keys(result);
+                    let j = keys.length;
 
-                while (j--) {
-                    memo[keys[j]] = result[keys[j]];
+                    while (j--) {
+                        memo[keys[j]] = result[keys[j]];
+                    }
                 }
             }
-        }
 
-        return memo;
-    }),
-    component = sequence(
-        find('<').not(find('</')).useCache(),
-        required(any(
-            tagName,
-            placeholder
-        )),
-        optional(sequence(
-            whiteSpace,
-            attrs
-        ).then(result => result[1])),
-        optionalWhiteSpace,
-        required(any(
-            find('/>').then(() => []).useCache(),
-            sequence(
-                required(find('>')).useCache(),
-                optional(repeat(any(
-                    whiteSpace,
-                    placeholder,
-                    textNode,
-                    deffered(() => component)
-                ))),
-                required(sequence(
-                    find('</'),
-                    any(
-                        tagName,
-                        placeholder
-                    ),
-                    optionalWhiteSpace,
-                    find('>')
-                )).useCache()
-            ).then(result => result[1] || [])
-        ))
-    ).then(result => ({
-        tag: result[1],
-        attrs: result[2] || {},
-        children: result[4] || []
-    })),
+            return memo;
+        }),
+        component = sequence(
+            find('<').not(find('</')),
+            required(any(
+                tagName,
+                placeholder
+            )),
+            optional(sequence(
+                whiteSpace,
+                attrs
+            ).then(result => result[1])),
+            optionalWhiteSpace,
+            required(any(
+                find('/>').then(() => []),
+                sequence(
+                    required(find('>')),
+                    optional(repeat(any(
+                        whiteSpace,
+                        placeholder,
+                        textNode,
+                        deffered(() => component)
+                    ))),
+                    required(sequence(
+                        find('</'),
+                        any(
+                            tagName,
+                            placeholder
+                        ),
+                        optionalWhiteSpace,
+                        find('>')
+                    )).useCache()
+                ).then(result => result[1] || [])
+            ))
+        ).then(result => ({
+            tag: result[1],
+            attrs: result[2] || {},
+            children: result[4] || []
+        }));
 
-    root = sequence(
+    return sequence(
         optionalWhiteSpace,
         component,
         optionalWhiteSpace,
         end()
     ).then(result => result[1]);
+}
+
+const root = getRoot(true),
+    rootNoCache = getRoot(false);
 
 function html(templates, ...values) {
     return root.parse(templates, values);
 }
 
+function htmlNoCache(templates, ...values) {
+    return rootNoCache.parse(templates, values);
+}
+
 export { html as default };
+export { htmlNoCache };
