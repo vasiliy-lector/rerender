f548f02cedd2ad6ad99a282c97dce2417fad8102
Vasiliy Lector <vasiliy.lector@gmail.com>
2017-01-02 16:45:48 +0300

    benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,32 +1,222 @@
 var Benchmark = require('benchmark'),
-    // t7 = require('t7'),
-    html = require('./lib/html.js').default,
-    htmlNoCache = require('./lib/html.js').htmlNoCache;
+    React = require('react'),
+    t7 = require('t7'),
+    es6x = require('es6x');
 
-var suite = new Benchmark.Suite,
-    templates = ['<div class="block" style=',' id="id1" title=','><p id="id2" class=','>text of p</p></div>'],
-    values = [{ background: 'red' }, 'title', 'classname'];
+// global.React = React;
+// t7.setOutput(t7.Outputs.React);
+// es6x.setOutputMethod(React.createElement);
 
-function cache() {
-    return html(templates, values[0], values[1], values[2]);
+var suite = new Benchmark.Suite;
+function es6xFn() {
+    return es6x `<div className="block" id="${'id1'}">
+        <form action="${'../'}">
+            <ul className='ulclass'>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value1'}"
+                        name=${'name1'}
+                        checked
+                    /> Some text 1
+                </li>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value2'}"
+                        name=${'name2'}
+                        checked
+                    /> Some text 2
+                </li>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value3'}"
+                        name=${'name3'}
+                        checked
+                    /> Some text 3
+                </li>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value4'}"
+                        name=${'name4'}
+                        checked
+                    /> Some text 4
+                </li>
+            </ul>
+        </form>
+    </div>`;
 }
 
-function noCache() {
-    return htmlNoCache(templates, values[0], values[1], values[2]);
+function t7Fn() {
+    return t7 `<div className="block" id="${'id1'}">
+        <form action="${'../'}">
+            <ul className='ulclass'>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value1'}"
+                        name=${'name1'}
+                        checked
+                    /> Some text 1
+                </li>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value2'}"
+                        name=${'name2'}
+                        checked
+                    /> Some text 2
+                </li>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value3'}"
+                        name=${'name3'}
+                        checked
+                    /> Some text 3
+                </li>
+                <li className=${'li1'}>
+                    <input type="checkbox"
+                        value="${'value4'}"
+                        name=${'name4'}
+                        checked
+                    /> Some text 4
+                </li>
+            </ul>
+        </form>
+    </div>`;
 }
 
-// function t7Fn() {
-//     return t7(templates, values[0], values[1], values[2]);
-// }
+function pure() {
+    return {
+        tag: 'div',
+        attrs: {
+            className: 'block',
+            id: 'id1'
+        },
+        children: [{
+            tag: 'form',
+            attrs: {
+                action: '../'
+            },
+            children: [{
+                tag: 'ul',
+                attrs: {
+                    className: 'ulclass'
+                },
+                children: [
+                    {
+                        tag: 'li',
+                        attrs: {
+                            className: 'li1'
+                        },
+                        children: [{
+                            tag: 'input',
+                            attrs: {
+                                type: 'checkbox',
+                                value: 'value1',
+                                name: 'name1',
+                                checked: true
+                            },
+                            children: ['Some text 1']
+                        }]
+                    },
+                    {
+                        tag: 'li',
+                        attrs: {
+                            className: 'li1'
+                        },
+                        children: [{
+                            tag: 'input',
+                            attrs: {
+                                type: 'checkbox',
+                                value: 'value1',
+                                name: 'name1',
+                                checked: true
+                            },
+                            children: ['Some text 1']
+                        }]
+                    },
+                    {
+                        tag: 'li',
+                        attrs: {
+                            className: 'li1'
+                        },
+                        children: [{
+                            tag: 'input',
+                            attrs: {
+                                type: 'checkbox',
+                                value: 'value1',
+                                name: 'name1',
+                                checked: true
+                            },
+                            children: ['Some text 1']
+                        }]
+                    },
+                    {
+                        tag: 'li',
+                        attrs: {
+                            className: 'li1'
+                        },
+                        children: [{
+                            tag: 'input',
+                            attrs: {
+                                type: 'checkbox',
+                                value: 'value1',
+                                name: 'name1',
+                                checked: true
+                            },
+                            children: ['Some text 1']
+                        }]
+                    }
+                ]
+            }]
+        }]
+    };
+}
 
-console.log('noCache', noCache()); // eslint-disable-line no-console
-console.log('cache', cache()); // eslint-disable-line no-console
-// console.log('t7Fn', t7Fn()); // eslint-disable-line no-console
+function pureReact() {
+    return React.createElement('div', { className: 'block', id: 'id1' },
+        React.createElement('form', { action: '../' },
+            React.createElement('ul', { className: 'ulclass' },
+                React.createElement('li', { className: 'li1' },
+                    React.createElement('input', { type: 'checkbox',
+                        value: 'value1',
+                        name: 'name1',
+                        checked: true
+                    }), 'Some text 1'
+                ),
+                React.createElement('li', { className: 'li1' },
+                    React.createElement('input', { type: 'checkbox',
+                        value: 'value1',
+                        name: 'name1',
+                        checked: true
+                    }), 'Some text 1'
+                ),
+                React.createElement('li', { className: 'li1' },
+                    React.createElement('input', { type: 'checkbox',
+                        value: 'value1',
+                        name: 'name1',
+                        checked: true
+                    }), 'Some text 1'
+                ),
+                React.createElement('li', { className: 'li1' },
+                    React.createElement('input', { type: 'checkbox',
+                        value: 'value1',
+                        name: 'name1',
+                        checked: true
+                    }), 'Some text 1'
+                )
+            )
+        )
+    );
+}
+
+console.log('es6xFn', es6xFn()); // eslint-disable-line no-console
+console.log('t7Fn', t7Fn()); // eslint-disable-line no-console
+console.log('pure', pure()); // eslint-disable-line no-console
+console.log('pureReact', pureReact()); // eslint-disable-line no-console
 
 suite
-.add('noCache', noCache)
-.add('cache', cache)
-// .add('t7Fn', t7Fn)
+.add('es6x', es6xFn)
+.add('t7', t7Fn)
+.add('pure', pure)
+.add('pureReact jsx', pureReact)
 .on('cycle', function(event) {
     console.log(String(event.target)); // eslint-disable-line no-console
 })
