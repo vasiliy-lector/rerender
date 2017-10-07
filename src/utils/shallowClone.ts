6d95d51306fc0cccaf27fb4bb6449ab05769417d
Vasiliy Lector <vasiliy.lector@gmail.com>
2017-10-07 10:50:59 +0300

    shallowClone ts fix

--- src/utils/shallowClone.ts
+++ src/utils/shallowClone.ts
@@ -1,9 +1,3 @@
-export function shallowClone(obj: any): typeof obj {
-    return Array.isArray(obj)
-        ? obj.map(item => item)
-        : Object.keys(obj).reduce((memo: typeof obj, name: string) => {
-            memo[name] = obj[name];
-
-            return memo;
-        }, {});
+export function shallowClone(obj: any[] | { [key: string]: any }) {
+    return Array.isArray(obj) ? [ ...obj ] : { ...obj };
 }
